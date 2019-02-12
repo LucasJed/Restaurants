fd7240cd62db2eb199b1422d9c5c7daf3eb9d9cf
augustefarge <auguste.farge.hei@gmail.fr>
2019-02-12 10:31:57 +0100

    complete restaurant class

--- app/src/main/java/hei/Descamps_Duclos_Farge/restaurants/MainActivity.java
+++ app/src/main/java/hei/Descamps_Duclos_Farge/restaurants/MainActivity.java
@@ -1,17 +1,31 @@
 package hei.Descamps_Duclos_Farge.restaurants;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressDialog dialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,3 +51,75 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    private class fetchURLTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(MainActivity.this);
+            dialog.setMessage("Downloading Data...");
+            dialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.d(TAG, "doInBackground 1");
+            try {
+                Log.d(TAG, "doInBackground 2");
+                return loadFromNetwork(strings[0]);
+            } catch (IOException e) {
+                Log.d(TAG, "doInBackground 3");
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(TAG, "onPostExecute");
+            super.onPostExecute(result);
+            buildPersonList(result);
+        }
+    }
+
+    private String loadFromNetwork(String urlString) throws IOException {
+        Log.d(TAG, "loadFromNetwork");
+        InputStream stream = null;
+        String str = "";
+        try {
+            stream = downloadURL(urlString);
+            str = readIt(stream);
+        } finally {
+            {
+                if (stream != null) {
+                    stream.close();
+                }
+            }
+        }
+        return str;
+    }
+
+    private InputStream downloadURL(String urlString) throws IOException {
+        Log.d(TAG, "downloadURL " + urlString);
+        URL url = new URL(urlString);
+        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+        //conn.setReadTimeout(10000);
+        //conn.setConnectTimeout(15000);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        conn.connect();
+        return conn.getInputStream();
+    }
+
+    private String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
+        Log.d(TAG, "readIt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder result = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            result.append(line);
+        }
+        return result.toString();
+    }
+
+}
--- app/src/main/java/hei/Descamps_Duclos_Farge/restaurants/Restaurant.java
+++ app/src/main/java/hei/Descamps_Duclos_Farge/restaurants/Restaurant.java
@@ -1,6 +1,34 @@
 package hei.Descamps_Duclos_Farge.restaurants;
 
 public class Restaurant {
+    private int id;
+    private String name;
+    private String address;
+    private String zip_code;
+    private String city;
+    private String country;
+    private String phone_number;
+    private float longitude;
+    private float lattitude;
+    private String logo;
+    private String cover;
+    private String description;
+
+    public Restaurant(int _id, String _name,String _address, String _zipCode, String _city,String _country, String _phoneNumber, float _longitude, float _lattitude, String _logo, String _cover, String _description) {
+        id = _id;
+        name=_name;
+        address=_address;
+        zip_code=_zipCode;
+        city=_city;
+        country=_country;
+        phone_number=_phoneNumber;
+        longitude=_longitude;
+        lattitude=_lattitude;
+        logo=_logo;
+        cover=_cover;
+        description=_description;
+
+    }
 
 
 }
